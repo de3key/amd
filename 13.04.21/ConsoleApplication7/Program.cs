2c0fe30cf15a7de61af957885062dc5ee5848c6a
de3key <gnosthard@gmail.com>
2021-04-13 11:34:29 +0700

    Да да да я

---
+++ 13.04.21/ConsoleApplication7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Число намбер уан");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите Число намбер ту");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите Число намбер фри");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+           if ( a > (b + c) &  b < (a + c) & c < (a + b))
+            {
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine(b);
+            }
+
+            Console.ReadKey(); 
+        }
+    }
+}
