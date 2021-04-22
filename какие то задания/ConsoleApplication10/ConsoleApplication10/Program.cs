e1177b4468a205889951511084cebc75d6b5e208
de3key <gnosthard@gmail.com>
2021-04-22 08:11:25 +0700

    натюрли

---
+++ какие то задания/ConsoleApplication10/ConsoleApplication10/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числовое значение оценки");
+            int a = Convert.ToInt32(Console.ReadLine());
+            switch (a)
+            {
+                case 2: { 
+                            Console.WriteLine("Неуд");
+                            break;
+                    }
+                 case 3:
+                    {
+                        Console.WriteLine("Уд");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Хорошо");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("Отлично");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Ошибка ввода данных");
+                        break;
+                    }
+               }
+
+            Console.ReadKey();
+
+            }        
+    }
+        
+}
---
+++ какие то задания/ConsoleApplication12/ConsoleApplication12/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите КОРОЧЕ ТУТ ЧТО ТО ЧТОБ Я РАДОВАЛСЯ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Понедельник");
+                        break;
+                    }
+
+                case 2:
+                    {
+                        Console.WriteLine("Вторник");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Cреда");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Четверг");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("Пятница");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("Суббота");
+                        break;
+                    }
+                case 7:
+                    {
+                        Console.WriteLine("Отдыхаем зяблики");
+                        break;
+                    }
+
+                    
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
